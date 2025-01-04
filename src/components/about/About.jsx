1d57e9f43d566c0bbca628bb19b7b7051565759a
Daniel Angulo <dangulo4@gmail.com>
2025-01-03 22:55:25 -0700

    updated image

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import Showcase from '../../img/showcase.jpg'
-import LinkedIn from '../../img/linkedIn.png'
-import Resume from '../../img/DanielAngulo.pdf'
+import React from 'react';
+import Showcase from '../../img/showcase.jpg';
+import LinkedIn from '../../img/cartoon.png';
+import Resume from '../../img/DanielAngulo.pdf';
 
-import './about.css'
+import './about.css';
 
 function about() {
   return (
-    <main className='about' id='about'>
+    <main
+      className='about'
+      id='about'
+    >
       <section className='about-left'>
         {/* <div className='about-card bg'></div> */}
         <div className='about-card'>
@@ -16,36 +19,53 @@ function about() {
             target='_blank'
             rel='noreferrer'
           >
-            <img src={LinkedIn} alt='showcase' className='about-image' />
+            <img
+              src={LinkedIn}
+              alt='showcase'
+              className='about-image'
+            />
           </a>
         </div>
       </section>
       <section className='about-right'>
         <h1 className='about-title'>About Me</h1>
-        <h2 className='resume' style={{ color: '#e9976a', font: '400' }}>
-          <a href={Resume} target='_blank' rel='noreferrer'>
+        <h2
+          className='resume'
+          style={{ color: '#e9976a', font: '400' }}
+        >
+          <a
+            href={Resume}
+            target='_blank'
+            rel='noreferrer'
+          >
             My Resume
           </a>
         </h2>
         <p className='about-sub'>
-          Fullstack developer from sunny San Diego, CA. I truly believe that
-          what makes us different makes us stronger. If I had to describe
-          myself, I am a person with a versatile skill-set, a lot of integrity,
-          and a willingness to bring value.
+          Fullstack developer from sunny San Diego, CA. I truly believe that what makes us different makes us stronger.
+          If I had to describe myself, I am a person with a versatile skill-set, a lot of integrity, and a willingness
+          to bring value.
         </p>
         <p className='about-desc'>
-          It has been my passion to become an expert in coding. I have a diverse
-          set of programming skills in full stack web application development. I
-          strive for clean content and simple structure with thoughtful design.
+          It has been my passion to become an expert in coding. I have a diverse set of programming skills in full stack
+          web application development. I strive for clean content and simple structure with thoughtful design.
         </p>
         <div className='about-award'>
-          <img src={Showcase} alt='' className='about-award-image' />
+          <img
+            src={Showcase}
+            alt=''
+            className='about-award-image'
+          />
           <div className='about-award-texts'>
             <h2
               className='about-award-title'
               style={{ color: '#e9976a', font: '400' }}
             >
-              <a href={Resume} target='_blank' rel='noreferrer'>
+              <a
+                href={Resume}
+                target='_blank'
+                rel='noreferrer'
+              >
                 My Resume
               </a>
             </h2>
@@ -53,7 +73,7 @@ function about() {
         </div>
       </section>
     </main>
-  )
+  );
 }
 
-export default about
+export default about;
